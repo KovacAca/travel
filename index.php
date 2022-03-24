584e228fc95a1ea531dcc59c5a5ceaa031d6140f
KovacAca <kovacaleksandar1@gmail.com>
2022-03-25 00:06:29 +0100

    destination

--- index.php
+++ index.php
@@ -7,7 +7,60 @@
   <button>Pretražite</button>
 </header>
 
-
-
+<!-- destination -->
+<section class="destination container">
+  <h2>Najpopularnije destinacije</h2>
+  <p>Rezervišite do kraja meseca za 30% popusta</p>
+  <article>
+    <div class="card">
+      <div class="img">
+        <img src="img/grcka.jpg" alt="">
+      </div>
+      <div class="text">
+        <i class="fas fa-map-marker-alt"></i><span> Grčka</span>
+      </div>
+    </div>
+    <div class="card">
+      <div>
+        <img src="img/italija.jpg" alt="">
+      </div>
+      <div class="text">
+        <i class="fas fa-map-marker-alt"></i><span> Italija</span>
+      </div>
+    </div>
+    <div class="card">
+      <div class="img">
+        <img src="img/kanada.jpg" alt="">
+      </div>
+      <div class="text">
+        <i class="fas fa-map-marker-alt"></i><span> Kanada</span>
+      </div>
+    </div>
+    <div class="card">
+      <div class="img">
+        <img src="img/norveska.jpg" alt="">
+      </div>
+      <div class="text">
+        <i class="fas fa-map-marker-alt"></i><span> Norveška</span>
+      </div>
+    </div>
+    <div class="card">
+      <div class="img">
+        <img src="img/rusija.jpg" alt="">
+      </div>
+      <div class="text">
+        <i class="fas fa-map-marker-alt"></i><span> Rusija</span>
+      </div>
+    </div>
+    <div class="card">
+      <div class="img">
+        <img src="img/spanija.jpg" alt="">
+      </div>
+      <div class="text">
+        <i class="fas fa-map-marker-alt"></i><span> Španija</span>
+      </div>
+    </div>
+  </article>
+</section>
 
 <?php include "footer.php"; ?>
