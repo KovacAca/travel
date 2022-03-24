f39693ed32de60be493de15180d7c2bb517cf2cf
KovacAca <kovacaleksandar1@gmail.com>
2022-03-25 00:24:16 +0100

    ponuda

---
+++ ponuda.php
@@ -0,0 +1,40 @@
+<?php include "header.php"; ?>
+
+<!-- header_ponuda -->
+<section class="header_ponuda">
+  <article class="container">
+    <h2>Popust 10-30%</h2>
+    <p>Uplata do januara 2022.</p>
+    <button>Pretražite</button>
+  </article>
+</section>
+
+<!-- ponuda -->
+<section class="ponuda container">
+  <h3>Iskoristite promotivne pakete</h3>
+  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum dolor harum, sapiente possimus molestiae voluptatum repellendus facere libero hic dolorum.</p>
+  <div class="wrap">
+    <article>
+      <img src="img/ponuda1.jpg" alt="">
+    </article>
+    <article>
+      <div>
+        <img src="img/ponuda2.jpg" alt="">
+      </div>
+      <div class="text">
+        <h4>Najbolje cene</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam doloremque alias illum accusantium laborum unde, quam quisquam sapiente molestiae debitis.</p>
+      </div>
+      <div class="text">
+        <h4>Najbolje lokacije</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam doloremque alias illum accusantium laborum unde, quam quisquam sapiente molestiae debitis.</p>
+      </div>
+      <div>
+        <img src="img/ponuda3.jpg" alt="">
+      </div>
+    </article>
+  </div>
+</section>
+
+<?php include "footer.php"; ?>
+  
